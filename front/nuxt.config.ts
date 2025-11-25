fd3a4513fe75e26c3b1866556a201c887adc17de
pcouy <contact@pierre-couy.dev>
2025-11-25 13:10:02 +0100

    front:Upgrade to Nuxt4

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: false },
-    css: ["~/assets/styles/main.scss"],
-    ssr: false,
-    telemetry: false,
-});
+  devtools: { enabled: false },
+  css: ["assets/styles/main.scss"],
+  ssr: false,
+  telemetry: false,
+  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  vite: {
+      server: {
+          allowedHosts: ["front"],
+      },
+  },
+  runtimeConfig: {
+      apiRoot: '/api/',
+  },
+})

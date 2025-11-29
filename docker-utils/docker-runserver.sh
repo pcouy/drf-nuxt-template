4b54a7ac5a99320b2fc87019b79d156d7733763a
pcouy <contact@pierre-couy.dev>
2025-11-29 14:55:14 +0100

    Use exec for faster stops/restarts

--- docker-utils/docker-runserver.sh
+++ docker-utils/docker-runserver.sh
@@ -2,4 +2,4 @@ cd /django_app
 /home/djangouser/.local/bin/poetry install --with dev
 /home/djangouser/.local/bin/poetry run python manage.py migrate
 /home/djangouser/.local/bin/poetry run python manage.py collectstatic --noinput
-/home/djangouser/.local/bin/poetry run python manage.py runserver_plus --nostatic 0.0.0.0:8000
+exec /home/djangouser/.local/bin/poetry run python manage.py runserver_plus --nostatic 0.0.0.0:8000
